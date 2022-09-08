b4ed58aed260c529896bbc4e2251fa9181ef8fd7
niconicorin <karsiong007@gmail.com>
2022-09-09 02:48:48 +0800

    test

--- test.js
+++ test.js
@@ -6,10 +6,11 @@ function utest1(ans){
 	console.log(res)
 	if(res == ans){
 		console.log("pass")
+		process.exit(0);
 	}else{
 		console.log("FAIL")
+		process.exit(1);
 	}
 }
 
 utest1(2)
-process.exit(1);
